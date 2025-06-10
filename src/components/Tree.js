b2112de1f3011efddb67713dcfea8f87d9f050ed
portiaportia <portiaplante@gmail.com>
2025-06-10 10:59:15 -0400

    awer

--- src/components/Tree.js
+++ src/components/Tree.js
@@ -1,13 +1,28 @@
 import "../css/tree.css";
+import {useState} from "react"
 
 
 const Tree = (props) => {
+    //variable is pruned it is initially set to false
+    //You can only change this state variable by calling pruneTree
+    const [pruned, pruneTree] = useState(false);
+
+    const doTreeJob = () => {
+        console.log("treeing")
+        pruneTree(true);
+    }
+
     return (
         <section className="tree">
             <h1>{props.name}</h1>
+            {pruned?
+                <p>Pruned</p>
+            :
+                <p>Not Pruned</p>    
+            }
             <p>{props.description}</p>
             <img src={props.image} />
-            <p className="prune-button-container"><button>Prue Tree</button></p>
+            <p className="prune-button-container"><button className={pruned?"disabled": ""} onClick={doTreeJob}>Prue Tree</button></p>
         </section>
     );
 }
