508ea2e633ecbe46c8ab16755f235d7b7e0cae3c
portiaportia <portiaplante@gmail.com>
2025-06-10 11:05:46 -0400

    awerwa

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,10 +1,18 @@
 import '../css/navigation.css';
 import {Link} from "react-router-dom";
+import {useState} from "react";
 
 const Navigation = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const toggleNav = () => {
+        setMenuOpen(!menuOpen);
+    }
+
     return (
         <nav id="main-nav">
-            <ul>
+            <button id="toggle-nav" onClick = {toggleNav}>...</button>
+            <ul class={menuOpen ? "" : "hide-small"}>
                 <li><Link to="/">Home</Link></li>
                 <li><Link to="/about">About</Link></li>
             </ul>
