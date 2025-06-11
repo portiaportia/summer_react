cb53831ca71a0c2e6f24c23eee828b000834bb7a
portiaportia <portiaplante@gmail.com>
2025-06-11 11:10:49 -0400

    contact form

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,9 +1,45 @@
+import react from "react";
+import {useState} from "react";
+
 const ContactForm = () => {
-    return (
-        <form id="contact-form">
-            <h2>sdafwe</h2>
-        </form>
-    );
+    const [contactResult, setContactResult] = useState("");
+
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        setContactResult("... Sending");
+        const formData = new FormData(event.target);
+    
+        formData.append("access_key", "YOUR_ACCESS_KEY_HERE");
+    
+        const object = Object.fromEntries(formData);
+        const json = JSON.stringify(object);
+    
+        const res = await fetch("https://api.web3forms.com/submit", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json"
+          },
+          body: json
+        }).then((res) => res.json());
+    
+        if (res.success) {
+          console.log("Success", res);
+          setContactResult("Email sent");
+        } else {
+            setContactResult("Sorry, your email couldn't be sent");
+        }
+      };
+    
+      return (
+          <form onSubmit={onSubmit}>
+            <input type="text" name="name"/>
+            <input type="email" name="email"/>
+            <textarea name="message"></textarea>
+            <button type="submit">Submit Form</button>
+            <div>{contactResult}</div>
+          </form>
+      );
 };
 
 export default ContactForm;
