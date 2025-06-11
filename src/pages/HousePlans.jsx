5324c772e42901bbc33d624994ebcaa0b9666f17
portiaportia <portiaplante@gmail.com>
2025-06-11 10:40:58 -0400

    awer

--- src/pages/HousePlans.jsx
+++ src/pages/HousePlans.jsx
@@ -1,13 +1,27 @@
 import '../css/houseplans.css';
 import HousePlan from "../components/HousePlan";
+import {useState, useEffect} from "react";
+import axios from "axios";
 
 const HousePlans = () => {
+    const [houses, setHouses] = useState([]);
+
+    //after teh page loads do our async retrieval of the houses
+    useEffect(()=>{
+        const loadHouses = async() => {
+            const response = await axios.get("https://portiaportia.github.io/json/house-plans.json");
+        };
+
+        loadHouses();
+    },[]);
+
     return (
         <>
             <div id="house-plans" className="columns">
-                <HousePlan />
-                <HousePlan />
-                <HousePlan />
+                {houses.map((house)=>(
+                    <HousePlan 
+                        kye="1"/>
+                ))}
             </div>
         </>
     );
